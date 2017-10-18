5264469a63e6eeb7b0231033094d6131145c31b1
Victor Barros <victor.barros@windowslive.com>
2017-10-18 21:50:16 -0200

    Created functions related to tournament project

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
@@ -13,65 +13,108 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
+    db = connect()
+    cur = db.cursor()
+    cur.execute("DELETE FROM matches;")
+    db.commit()
+    db.close()
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    db = connect()
+    cur = db.cursor()
+    cur.execute("DELETE FROM players;")
+    db.commit()
+    db.close()
 
 
 def countPlayers():
-    """Returns the number of players currently registered."""
+    """Return the number of players currently registered."""
+    db = connect()
+    cur = db.cursor()
+    cur.execute("SELECT COUNT(*) AS num FROM players;")
+    _count = cur.fetchone()
+    db.close()
+    return _count[0]
 
 
 def registerPlayer(name):
-    """Adds a player to the tournament database.
-  
+    """Add a player to the tournament database.
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
     Args:
       name: the player's full name (need not be unique).
+
     """
+    db = connect()
+    cur = db.cursor()
+    cur.execute("INSERT INTO players (name) VALUES (%s);", (name,))
+    db.commit()
+    db.close()
 
 
 def playerStandings():
-    """Returns a list of the players and their win records, sorted by wins.
+    """Return a list of the players and their win records, sorted by wins.
 
     The first entry in the list should be the player in first place, or a player
     tied for first place if there is currently a tie.
-
     Returns:
       A list of tuples, each of which contains (id, name, wins, matches):
         id: the player's unique id (assigned by the database)
         name: the player's full name (as registered)
         wins: the number of matches the player has won
         matches: the number of matches the player has played
+
     """
+    db = connect()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM standings")
+    _standings = cur.fetchall()
+    db.close()
+
+    return _standings
 
 
 def reportMatch(winner, loser):
-    """Records the outcome of a single match between two players.
+    """Record the outcome of a single match between two players.
 
     Args:
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
+
     """
- 
- 
+    db = connect()
+    cur = db.cursor()
+    cur.execute("INSERT INTO matches (winner, loser) VALUES ( \
+        {0}, {1});".format(winner, loser))
+    db.commit()
+    db.close()
+
+
 def swissPairings():
-    """Returns a list of pairs of players for the next round of a match.
-  
+    """Return a list of pairs of players for the next round of a match.
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
         name1: the first player's name
         id2: the second player's unique id
         name2: the second player's name
+
     """
+    db = connect()
+    cur = db.cursor()
+    cur.execute("SELECT player.id, player.name, opponent.id, opponent.name \
+        FROM standings player, standings opponent \
+        WHERE player.wins = opponent.wins \
+        AND player.id < opponent.id;")
+    _pairs = cur.fetchall()
+    db.close()
 
-
+    return _pairs
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,33 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+DROP TABLE IF EXISTS players CASCADE;
+DROP TABLE IF EXISTS matches CASCADE;
 
+CREATE TABLE players (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE matches (
+  id SERIAL PRIMARY KEY,
+  winner INT REFERENCES players(id),
+  loser INT REFERENCES players(id)
+);
+
+CREATE VIEW count AS
+  SELECT players.id, count(matches.winner) AS n FROM players
+  LEFT JOIN matches ON players.id = matches.winner
+  OR players.id = matches.loser
+  GROUP BY players.id;
+
+CREATE VIEW wins AS
+  SELECT players.id, count(matches.winner) AS n FROM players
+  LEFT JOIN matches ON players.id =  matches.winner
+  GROUP BY players.id;
+
+CREATE VIEW standings AS
+  SELECT players.id, players.name, wins.n AS wins, count.n AS matches_played
+      FROM players, wins, count
+      WHERE players.id = wins.id and players.id = count.id
+      ORDER BY wins DESC;
